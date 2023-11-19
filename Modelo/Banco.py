ef848112855832f6d9c980e728ccd1ea5c2b6ca1
Marcos Rodriguez <kronossnow5544@gmail.com>
2023-11-19 11:09:20 -0500

    CRUD terminado

--- Modelo/Banco.py
+++ Modelo/Banco.py
@@ -12,6 +12,61 @@ class Banco():
         self.__listaCliente = []
         self.__listaComercial = []
 
+
+    #Funciones Auxiliares
+    def buscarCliente(self, ci):
+        for i in range(len(self.__listaCliente)):
+            if self.__listaCliente[i].ci == ci:
+                return i
+        return None
+    
+    def buscarComercial(self, ci):
+        for i in range(len(self.__listaComercial)):
+            if self.__listaComercial[i].ci == ci:
+                return i
+        return None
+    
+    def actCIClienteCuenta(self, ci_ant, ci_nuevo):
+        for i in range(len(self.__listaCuentaSimple)):
+            if self.__listaCuentaSimple[i].cliente == ci_ant:
+                self.__listaCuentaSimple[i].cliente = ci_nuevo
+        for i in range(len(self.__listaCuentaPF)):
+            if self.__listaCuentaPF[i].cliente == ci_ant:
+                self.__listaCuentaPF[i].cliente = ci_nuevo
+        for i in range(len(self.__listaCuentaFF)):
+            if self.__listaCuentaFF[i].cliente == ci_ant:
+                self.__listaCuentaFF[i].cliente = ci_nuevo 
+
+    def actCIComercialCuenta(self, ci_ant, ci_nuevo):
+        for i in range(len(self.__listaCuentaSimple)):
+            if self.__listaCuentaSimple[i].datos_comercial == ci_ant:
+                self.__listaCuentaSimple[i].datos_comercial = ci_nuevo
+        for i in range(len(self.__listaCuentaPF)):
+            if self.__listaCuentaPF[i].datos_comercial == ci_ant:
+                self.__listaCuentaPF[i].datos_comercial = ci_nuevo
+        for i in range(len(self.__listaCuentaFF)):
+            if self.__listaCuentaFF[i].datos_comercial == ci_ant:
+                self.__listaCuentaFF[i].datos_comercial = ci_nuevo        
+
+    def buscarCuentaSimple(self, num):
+        for i in range(len(self.__listaCuentaSimple)):
+            if self.__listaCuentaSimple[i].num_cuenta == num:
+                return i
+        return None
+
+    def buscarCuentaPF(self, num):
+        for i in range(len(self.__listaCuentaPF)):
+            if self.__listaCuentaPF[i].num_cuenta == num:
+                return i
+        return None
+
+
+    def buscarCuentaFF(self, num):
+        for i in range(len(self.__listaCuentaFF)):
+            if self.__listaCuentaFF[i].num_cuenta == num:
+                return i
+        return None
+
     #CRUD: Create, Read, Update, Delete
     
     #Create
@@ -45,6 +100,9 @@ class Banco():
     
     def eliminarCliente(self, ci):
         self.__listaCliente = list(filter(lambda x: x.ci != ci, self.__listaCliente))
+        self.__listaCuentaSimple = list(filter(lambda x: x.cliente != ci, self.__listaCuentaSimple))
+        self.__listaCuentaPF = list(filter(lambda x: x.cliente != ci, self.__listaCuentaPF))
+        self.__listaCuentaFF = list(filter(lambda x: x.cliente != ci, self.__listaCuentaFF))
 
     def eliminarComercial(self, ci):
         self.__listaComercial = list(filter(lambda x: x.ci != ci, self.__listaComercial))
@@ -70,7 +128,75 @@ class Banco():
     def listaComercial(self):
         return self.__listaComercial
     
-    #Modify
+    #Update
+    def actualizarCliente(self, ci, cliente):
+        indice = self.buscarCliente(ci)
+        if indice == None:
+            raise Exception("No existe el cliente que desea modificar")
+        ind_comp = self.buscarCliente(cliente.ci)
+        if(ind_comp == indice):
+            self.__listaCliente[indice] = cliente
+        elif(ind_comp == None):
+            self.actCIClienteCuenta(ci, cliente.ci)
+            self.__listaCliente[indice] = cliente
+        else:
+            raise Exception("El cliente ya existe en el repositorio")
+        
+    def actualizarComercial(self, ci, comercial):
+        indice = self.buscarComercial(ci)
+        if indice == None:
+            raise Exception("No existe el comercial que desea modificar")
+        ind_comp = self.buscarComercial(comercial.ci)
+        if (ind_comp == indice):
+            self.__listaComercial[indice] = comercial
+        elif (ind_comp == None):
+            self.actCIComercialCuenta(ci, comercial.ci)
+            self.__listaComercial[indice] = comercial
+        else:
+            raise Exception("El comercial ya existe en el repositorio")
+        
+    def actualizarCuentaSimple(self, num, cuenta_simp):
+        indice = self.buscarCuentaSimple(num)
+        if indice == None:
+            raise Exception("No existe la cuenta que desea modificar")
+        ind_comp = self.buscarCuentaSimple(cuenta_simp.num_cuenta)
+        ind_comp2 = self.buscarCuentaPF(cuenta_simp.num_cuenta)
+        ind_comp3 = self.buscarCuentaFF(cuenta_simp.num_cuenta)
+        if (ind_comp3 != None) or (ind_comp2 != None):
+            raise Exception("Ya existe una cuenta de otro tipo con este número")
+        elif (ind_comp == indice) or (ind_comp == None):
+            self.__listaCuentaSimple[indice] = cuenta_simp
+        else:
+            raise Exception("La cuenta ya existe en el repositorio")
+
+
+    def actualizarCuentaPF(self, num, cuenta_pf):
+        indice = self.buscarCuentaPF(num)
+        if indice == None:
+            raise Exception("No existe la cuenta que desea modificar")
+        ind_comp2 = self.buscarCuentaSimple(cuenta_pf.num_cuenta)
+        ind_comp = self.buscarCuentaPF(cuenta_pf.num_cuenta)
+        ind_comp3 = self.buscarCuentaFF(cuenta_pf.num_cuenta)
+        if (ind_comp3 != None) or (ind_comp2 != None):
+            raise Exception("Ya existe una cuenta de otro tipo con este número")
+        elif (ind_comp == indice) or (ind_comp == None):
+            self.__listaCuentaPF[indice] = cuenta_pf
+        else:
+            raise Exception("La cuenta ya existe en el repositorio")
+
+    def actualizarCuentaFF(self, num, cuenta_ff):
+        indice = self.buscarCuentaFF(num)
+        if indice == None:
+            raise Exception("No existe la cuenta que desea modificar")
+        ind_comp2 = self.buscarCuentaSimple(cuenta_ff.num_cuenta)
+        ind_comp3 = self.buscarCuentaPF(cuenta_ff.num_cuenta)
+        ind_comp = self.buscarCuentaFF(cuenta_ff.num_cuenta)
+        if (ind_comp3 != None) or (ind_comp2 != None):
+            raise Exception("Ya existe una cuenta de otro tipo con este número")
+        elif (ind_comp == indice) or (ind_comp == None):
+            self.__listaCuentaFF[indice] = cuenta_ff
+        else:
+            raise Exception("La cuenta ya existe en el repositorio")
 
 
     #Cargar y guardar desde Bases de Datos .txt
@@ -224,6 +350,3 @@ class Banco():
                 anios_ex = int(lista[3].strip())
                 comercial = Comercial(nombre, sexo, ci, anios_ex)
                 self.ingresarComercial(comercial)
-                
-
-
